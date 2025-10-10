15ca4f366413bab3ecc303baad66fc676abfbc9c
Arajit-Pati <arajitpati2004@gmail.com>
2025-10-10 22:57:33 +0530

    minor post view improvement

--- website/views.py
+++ website/views.py
@@ -54,5 +54,5 @@ def posts(username):
         flash("User does not exist.", category="error")
         return redirect(url_for("views.home"))
 
-    posts = Post.query.filter_by(author=user.id).all()
+    posts = user.posts
     return render_template("posts.html", user=current_user, posts=posts, username=username)
